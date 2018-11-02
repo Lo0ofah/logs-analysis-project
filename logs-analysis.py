ec89fdbf5b84440bd942d3d400fe808b56d44d52
Elaaf Zaker <miss.lo0ofah@gmail.com>
2018-11-02 20:07:31 +0300

    First query

--- logs-analysis.py
+++ logs-analysis.py
@@ -1,6 +1,10 @@
 import psycopg2
 
 DBName = "news"
+Question = ["What are the most popular three articles of all time?",
+           "Who are the most popular article authors of all time?",
+           "On which days did more than 1% of requests lead to errors?"
+          ]
 
 def executeQuery(query):
     db = psycopg2.connect(database=DBName)
@@ -10,5 +14,13 @@ def executeQuery(query):
     db.close()
     return result
 
-firstQuery = "select * from authors;"
+firstQuery = """select title, count(*) as numberOfView
+                from articles,log
+                where articles.slug = substring(log.path, 10)
+                group by articles.title
+                order by numberOfView  desc
+                limit 3;
+            """
+
+print(Question[0])
 print(executeQuery(firstQuery))
